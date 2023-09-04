eb7971e3f375be4b1693da2c7d69dd05d19aa677
Christian Hizon <ch@2doit.nu>
2023-09-04 14:43:33 +0800

    updates

---
+++ inventory_package/lib/domain/entities/transaction.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'transaction.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Transaction _$$_TransactionFromJson(Map<String, dynamic> json) =>
+    _$_Transaction(
+      id: json['id'] as String,
+      collectionId: json['collectionId'] as String,
+      collectionName: json['collectionName'] as String,
+      created: DateTime.parse(json['created'] as String),
+      updated: DateTime.parse(json['updated'] as String),
+      quantity: json['quantity'] as int,
+      unitPrice: (json['unitPrice'] as num).toDouble(),
+      customerName: json['customerName'] as String,
+      isVoided: json['isVoided'] as bool? ?? false,
+    );
+
+Map<String, dynamic> _$$_TransactionToJson(_$_Transaction instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'collectionId': instance.collectionId,
+      'collectionName': instance.collectionName,
+      'created': instance.created.toIso8601String(),
+      'updated': instance.updated.toIso8601String(),
+      'quantity': instance.quantity,
+      'unitPrice': instance.unitPrice,
+      'customerName': instance.customerName,
+      'isVoided': instance.isVoided,
+    };
